d56f949dc13609d2376fc0daf6bd59d70f3fb1b2
isicheri <tech2dom@gmail.com>
2023-02-21 12:27:28 +0100

    made little changes in the config files

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 export const Config = {
     DATABASE: process.env.DB || 'lms-nupat',
     DATABASE_USER: process.env.DB_USER || 'root',
-    DATABASE_PASSWORD: process.env.DB_PASSWORD || '',
+    DATABASE_PASSWORD: process.env.DB_PASSWORD || 'dominion123#',
     PORT: process.env.PORT || 8080,
     JwtPass: process.env.JwtPass || 'e893jhqnoe09eiqjoe'
 }
--- src/models/userModel/users.model.js
+++ src/models/userModel/users.model.js
@@ -2,18 +2,26 @@ import { DataTypes } from "sequelize";
 import { sequelize } from "../../../databases.js";
 
 export const User = sequelize.define('user',{
+    id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4 
+    },
     firstName: {
         type: DataTypes.STRING,
         allowNull: false
     },
-    surname: {
+    lastName: {
         type: DataTypes.STRING,
         allowNull: false
     },
     email: {
         type: DataTypes.STRING,
         allowNull: false,
-        unique: true
+        unique: true,
+        validate: {
+            isEmail: true
+        }
     },
     phoneNumber: {
         type: DataTypes.INTEGER,
